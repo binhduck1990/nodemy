32c65dc2e1681bff3c5e1c1306c69ad0bbd1a003
binh <an duong>
2020-12-05 18:43:59 +0700

    test api

--- my-app/test.js
+++ my-app/test.js
@@ -26,25 +26,43 @@ testjs2 = () => {
     })
   }
 
-testjs3 = () => {
-    const data = fetch('http://localhost:3000/api/post')
-    .then(response => response.json())
-    .then(data => fetch('http://localhost:3000/api/user'))
-    .then(response2 => response2.json())
-    .then(data2 => console.log(data2));
+testApi1 = async () => {
+    const response = await fetch('http://localhost:3000/api/post')
+    const posts = await response.json()
+    return posts
 }
 
-const p = async () => {
+testApi2 = async () => {
+    const response = await fetch('http://localhost:3000/api/user')
+    const users = await response.json()
+    return users
+}
+
+const testPromise = async () => {
     const timeStart = performance.now()
-    const [test1, test2] = await Promise.all([testjs1(), testjs2() ,testjs2().then])
-    const test3 = await test2.then
+    const [test1, test2, test3] = await Promise.all([testjs1(), testjs2() ,testjs2().then])
     const timeEnd = performance.now() - timeStart
     console.log('timeEnd', timeEnd)
     return [test1, test2, test3]
   }
 
-  p().then(data => {
+  testPromise().then(data => {
     console.log(data)
   })
 
-//   testjs3()
+  const testApi = async () => {
+    const timeStart = performance.now()
+    // const test1 = await testApi1()
+    // const test2 = await testApi2()
+    const [test1, test2] = await Promise.all([testApi1(), testApi2()])
+    const timeEnd = performance.now() - timeStart
+    console.log(timeEnd)
+    return {
+        posts: test1,
+        users: test2
+    }
+  }
+
+  testApi().then(data => {
+      console.log('123')
+  })
